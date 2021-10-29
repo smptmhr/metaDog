1c3ae69994c0a147eb959ab1e89a7eb3e31f6e27
Nakamura <k_nakamura@vrlab.esys.tsukuba.ac.jp>
2021-10-29 19:09:54 +0900

    なで検出用M5Atomプログラム

---
+++ Serial_M5Atom-Unity/LedArray.h
@@ -0,0 +1,10 @@
+#define COLOR_RED 0xff0000
+#define COLOR_BLUE 0x0000ff
+#define COLOR_BLACK 0x000000
+#define COLOR_WHITE 0xffffff
+
+int arrayCircle_Red[] = {1,2,3,5,9,10,14,15,19,21,22,23};
+int arrayCircle_Black[] = {0,4,6,7,8,11,12,13,16,17,18,20,24};
+
+int arrayCross_Blue[] = {0,4,6,8,12,16,18,20,24};
+int arrayCross_Black[] = {1,2,3,5,7,9,10,11,13,14,15,17,19,21,22,23};
